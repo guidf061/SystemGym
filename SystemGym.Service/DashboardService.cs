b2c07174a494953edb4a55eca26247763d97b54d
Guilherme Santana da Silva <g.santana18@hotmail.com>
2019-11-10 02:12:38 -0300

    .

--- SystemGym.Service/DashboardService.cs
+++ SystemGym.Service/DashboardService.cs
@@ -28,6 +28,7 @@ namespace SystemGym.Service
 
             var quantityModel = new DashboardQuantityReturnModel
             {
+
                 Janeiro = this.context.Aluno
                 .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.Month) && x.Ativo.Equals(true))
                 .Count(),
@@ -38,6 +39,42 @@ namespace SystemGym.Service
 
                 Marco = this.context.Aluno
                 .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(2)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Abriu = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(3)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Maio = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(4)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Junho = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(5)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Julho = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(6)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Agosto = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(7)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Setembro = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(8)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Outubro = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(9)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Novembro = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(10)) && x.Ativo.Equals(true))
+                .Count(),
+
+                Dezembro = this.context.Aluno
+                .Where(x => x.Pessoa.CriacaoData.Value.Month.Equals(janeiro.AddMonths(11)) && x.Ativo.Equals(true))
                 .Count(),
             };
 
